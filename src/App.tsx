c30a98a0b1fc71bd332230a7699cbb164f310af2
Daniele Guidetti <daniele.guidetti@nurale.it>
2025-10-16 12:27:08 +0200

    Added login redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 
+import AuthProvider from "./contexts/AuthContext";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,22 @@
+import { useAuth } from "@/contexts/AuthContext";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+
 const Home: React.FC = () => {
-  return <main>home</main>;
+  const navigate = useNavigate();
+
+  const { auth } = useAuth();
+  const { isAuthenticated, username } = auth;
+
+  useEffect(() => {
+    if (!isAuthenticated) navigate("/login");
+  }, []);
+
+  return (
+    <div>
+      <p>Benvenutom, {username}</p>
+    </div>
+  );
 };
 
 export default Home;
