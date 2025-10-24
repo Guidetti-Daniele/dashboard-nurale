5bc9a4b05ffd1d28e0b19390e2ffdb82aa451187
GuidettiDanieleNurale <daniele.guidetti@nurale.it>
2025-10-24 09:19:09 +0200

    Removed Welcome component in pages

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,13 @@
-import { Welcome } from "@/pages";
+import { useAuth } from "@/hooks";
 
 export const Home: React.FC = () => {
-  return <Welcome />;
+  const {
+    auth: { username },
+  } = useAuth();
+
+  return (
+    <div className="h-32 flex justify-center items-center">
+      <p className="text-3xl text-black text-center">Benvenuto, {username}</p>
+    </div>
+  );
 };
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,18 +1,6 @@
-// Home
 export * from "./Home/Home";
-export * from "./Home/Welcome";
-
-// Calendar
 export * from "./Calendar/Calendar";
-
-// Inbox
 export * from "./Inbox/Inbox";
-
-// Login
 export * from "./Login/Login";
-
-// Search
 export * from "./Search/Search";
-
-// Settings
 export * from "./Settings/Settings";
